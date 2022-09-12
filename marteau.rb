f865e331d9bc0979b8bbae674b1153ad0fa5bcfe
Marquis Kurt <software@marquiskurt.net>
2022-09-12 13:31:11 -0400

    :rocket: Update marteau => 0.1.0alpha4
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- marteau.rb
+++ marteau.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Marteau < Formula
   homepage 'https://github.com/Indexing-Your-Heart/marteau'
-  version '0.1.0-alpha3'
-  url 'https://github.com/Indexing-Your-Heart/marteau/archive/0.1.0-alpha3.tar.gz'
-  sha256 'fae26178f382d882457a7af16924e9bcb25a60d4b2a869204205ee0cb69e8167'
+  version '0.1.0-alpha4'
+  url 'https://github.com/Indexing-Your-Heart/marteau/archive/0.1.0-alpha4.tar.gz'
+  sha256 'f4d174bddf1df9537a8254908f30eb8dbb5a9098194c5d6b7d9f4753d6952e99'
 
   head 'https://github.com/Indexing-Your-Heart/marteau.git'
 
