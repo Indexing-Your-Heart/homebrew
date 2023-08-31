6ace241e5c36ecdf73c4dae1730ebb91b3a0a01f
Marquis Kurt <software@marquiskurt.net>
2023-08-31 17:31:51 -0400

    :rocket: IOH-23: Update marteau 0.2.0-alpha1

--- marteau.rb
+++ marteau.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Marteau < Formula
   homepage 'https://github.com/Indexing-Your-Heart/marteau'
   version '0.1.0-alpha4'
-  url 'https://github.com/Indexing-Your-Heart/marteau/archive/0.1.0-alpha4.tar.gz'
-  sha256 'f4d174bddf1df9537a8254908f30eb8dbb5a9098194c5d6b7d9f4753d6952e99'
+  url 'https://github.com/Indexing-Your-Heart/marteau/archive/0.2.0-alpha1.tar.gz'
+  sha256 'efcfa705933d3d9af35703203e65205c5aa410f0812eed041161c6095d70132d'
 
   head 'https://github.com/Indexing-Your-Heart/marteau.git'
 
